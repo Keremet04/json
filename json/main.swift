c43ef649db37d118f224c102eaa6f8d0bb5d4b72
Keremet Dogdurbaeva <dogdurbaeva04@bk.ru>
2023-01-05 16:32:18 +0600

    h

--- json/main.swift
+++ json/main.swift
@@ -7,5 +7,63 @@
 
 import Foundation
 
-print("Hello, World!")
+let json = """
+[
+{
+    "name": "Keremet",
+    "age": 18,
+    "profession": "IOS - developer",
+    
+    "company": {
+        "companyName": "GeekTech",
+        "studentsCount": 44000.454
+      },
 
+     "Best_Student": "GeekTech Student #1"
+  }
+]
+"""
+
+struct Company: Decodable {
+    let companyName: String
+    let studentsCount: Double
+}
+
+struct Developer: Decodable {
+    let name: String
+    let age: Int
+    let profession: String
+    let company: Company
+    let bestStudent: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case age
+        case profession
+        case company
+        case bestStudent = "Best_Student"
+    }
+}
+ 
+class SomeController {
+    private var persons: [Developer] = [] {
+        didSet {
+            print(persons)
+        }
+    }
+    
+    func parseJSON() {
+        let personData = Data(json.utf8)
+        let decoder = JSONDecoder()
+        
+        do {
+            let convertedData = try decoder.decode([Developer].self, from: personData)
+            persons = convertedData
+        } catch {
+            print("Error is: \(error.localizedDescription)")
+        }
+    }
+}
+
+let controller = SomeController()
+controller.parseJSON()
